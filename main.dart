13a5cd9951ee43a6ed266ac6f8c38d6a3ee599b8
omwerialex <88056151+omwerialex@users.noreply.github.com>
2021-07-27 14:25:33 -0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+bool connected;
+
+class MyApp extends StatelessWidget {
+  final title = 'Flutter BLE Scan Demo';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      home: MyHomePage(title: title),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+  List<ScanResult> scanResultList = [];
+  bool _isScanning = false;
+  @override
+  initState() {
+    super.initState();
+    initBle();
+  }
+
+  void initBle() {
+    flutterBlue.isScanning.listen((isScanning) {
+      _isScanning = isScanning;
+      setState(() {});
+    });
+  }
+
+  scan() async {
+    if (!_isScanning) {
+      scanResultList.clear();
+
+      flutterBlue.startScan(timeout: Duration(seconds: 4));
+
+      flutterBlue.scanResults.listen((results) {
+        scanResultList = results;
+
+        setState(() {});
+      });
+    } else {
+      flutterBlue.stopScan();
+    }
+  }
+
+  Widget deviceSignal(ScanResult r) {
+    return Text(r.rssi.toString());
+  }
+
+  Widget deviceMacAddress(ScanResult r) {
+    return Text(r.device.id.id);
+  }
+
+  Widget deviceName(ScanResult r) {
+    String name = r.device.name;
+
+    if (r.device.name.isNotEmpty) {
+      name = r.device.name;
+    } else if (r.advertisementData.localName.isNotEmpty) {
+      name = r.advertisementData.localName;
+    } else {
+      name = 'N/A';
+    }
+    return Text(name);
+  }
+
+  Widget leading(ScanResult r) {
+    return CircleAvatar(
+        child: Icon(
+          Icons.bluetooth,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.purple[200]);
+  }
+
+  void onTap(ScanResult r) {
+    if (connected != true) {
+      r.device.connect();
+      connected = true;
+      print('${r.device.name}');
+      print('device connected');
+      print(r.device.isDiscoveringServices);
+    } else {
+      r.device.disconnect();
+      connected = false;
+      print('device disconnected');
+    }
+  }
+
+  Widget listItem(ScanResult r) {
+    return ListTile(
+      onTap: () => onTap(r),
+      leading: leading(r),
+      title: deviceName(r),
+      subtitle: deviceMacAddress(r),
+      trailing: deviceSignal(r),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: ListView.separated(
+          itemCount: scanResultList.length,
+          itemBuilder: (context, index) {
+            return listItem(scanResultList[index]);
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return Divider();
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: scan,
+        child: Icon(_isScanning ? Icons.stop : Icons.search),
+      ),
+    );
+  }
+}
